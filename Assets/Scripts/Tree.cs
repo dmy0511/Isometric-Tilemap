1bb83bcf151317e1fb48155984c7f03ec1d70afa
dmy0511 <127921600+dmy0511@users.noreply.github.com>
2023-06-11 16:38:41 +0900

    사과 수확 & 획득 버튼 작동
    획득 누르면 인벤토리에 사과 이미지 들어가게는 했는데 각 슬롯마다 들어가게는 못함. 아무튼 그럼.

---
+++ Assets/Scripts/Tree.cs
@@ -0,0 +1,84 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class Tree : MonoBehaviour
+{
+    public GameObject treePanel;
+    public GameObject GetPanel;
+
+    public Slider treeHp;
+
+    private float maxHp = 100;
+    private float curHp = 100;
+
+    //public Image itemImage;
+    //public Image inventorySlot;
+
+    void Start()
+    {
+        Time.timeScale = 1.0f;
+
+        treeHp.value = (float)curHp / (float)maxHp;
+    }
+
+    void Update()
+    {
+        if (curHp <= 0)
+        {
+            treePanel.gameObject.SetActive(false);
+            GetPanel.SetActive(true);
+            Time.timeScale = 0.0f;
+            curHp += 100;
+        }
+
+        HandleHp();
+    }
+
+    private void HandleHp()
+    {
+        treeHp.value = (float)curHp / (float)maxHp;
+    }
+
+    public void attack()
+    {
+        if(curHp > 0)
+        {
+            curHp -= 20;
+        }
+        else
+        {
+            curHp = 0;
+        }    
+    }
+
+    public void get()
+    {
+        Time.timeScale = 1.0f;
+        GetPanel.SetActive(false);
+
+        //Sprite itemSprite = itemImage.sprite;
+
+        //inventorySlot.sprite = itemSprite;
+
+        //inventorySlot.enabled = true;
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            treePanel.gameObject.SetActive(true);
+        }
+    }
+
+    private void OnCollisionExit2D(Collision2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            treePanel.gameObject.SetActive(false);
+        }
+    }
+}
